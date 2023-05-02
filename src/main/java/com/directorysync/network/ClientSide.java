38e8109ac345680d2b6156b4fbf5298b7ddc8991
Tartiine <sofia.saadi6@gmail.com>
2023-05-02 21:42:54 +0200

    Updating ClientSide

--- src/main/java/com/directorysync/network/ClientSide.java
+++ src/main/java/com/directorysync/network/ClientSide.java
@@ -10,22 +10,16 @@ import java.nio.file.WatchEvent.Kind;
 
 public class ClientSide{
 
-	static String address = "192.168.1.254";
+	static String address = "192.168.1.69";
 	public static Path dir = Paths.get("./trgDir");
 
     public static void main(String[] args) throws IOException {
 
-		try (Socket socket = new Socket(address, 3456)) {
-			dirExist(dir);
-			socket.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		dirExist(dir);
 
-            while (true) {
-				
-                // Keep the client-side code running until the program is closed
-            }
+		while (true) {
+			
+		}
 
     }
 
@@ -48,11 +42,14 @@ public class ClientSide{
 	}
 
 	public static void dirExist(Path file) throws IOException {
-		boolean exists = Files.exists(file);
-		if (exists) {
-			System.out.println("Path exists!");
-		} else {
-			System.out.println("Path does not exist.");
+		try (Socket socket = new Socket(address, 3456)) {
+			String message = "checking" + " " + file.toString();
+			OutputStream outputStream = socket.getOutputStream();
+			outputStream.write(message.getBytes());
+			outputStream.flush();
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
